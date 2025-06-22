0912420b618238efd75a6c21d6713c53005ffffd
BSoybilgen <baris.soybilgen@samblagroup.com>
2025-06-22 14:28:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="simple_trade",
     description="Compute technical indicators and build trade strategies in a simple way",
     license='AGPL-3.0',
-    version="0.1.2",
+    version="0.1.3",
     author="Baris Soybilgen",
     packages=find_packages(),
     project_urls={
